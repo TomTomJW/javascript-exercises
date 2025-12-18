864cd1a81dfa314395de983e62c04c5144dfdd3c
Tom W <199689467+TomTomJW@users.noreply.github.com>
2025-12-17 23:23:16 -0500

    Finished 06-07

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, input) {
+    if (input < 0) {
+        return 'ERROR'
+    }
+    let value = '';
+    for (let i = 0; i < input; i++) {
+        value = value + string;
+    }
+    return value;
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let listOfChars = inputString.split('');
+    let reversedListOfChars = listOfChars.reverse();
+    let reversedString = reversedListOfChars.join('');
+    return reversedString;
 };
 
 // Do not edit below this line
