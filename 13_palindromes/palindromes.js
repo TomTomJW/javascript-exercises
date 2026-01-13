f0283969c874349e35b160d899e424a0291505bc
Tom W <199689467+TomTomJW@users.noreply.github.com>
2026-01-12 21:35:04 -0500

    Finished 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    let lowerCase = inputString.toLowerCase();
+    let noSpaces = lowerCase.replaceAll(" ", "");
+    let noPunctuation = noSpaces.replaceAll("!", "").replaceAll(".", "").replaceAll("?", "").replaceAll(",", "").replaceAll(":", "").replaceAll(";", "");
+    for (let i = 0; i < noPunctuation.length; i++) {
+        if (noPunctuation.charAt(i) != noPunctuation.charAt((noPunctuation.length - 1) - i)) {
+            return false;
+        }
+    }
+    return true;
 };
+
+console.log("testing");
 
 // Do not edit below this line
 module.exports = palindromes;
