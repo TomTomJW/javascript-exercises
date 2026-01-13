4c60e45ee4119fc49d20f53c1c4004551ef0914b
Tom W <199689467+TomTomJW@users.noreply.github.com>
2026-01-13 12:23:31 -0500

    Finished 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(inputArray) {
+    inputArray.forEach(item => {
+        if (!item.hasOwnProperty('yearOfDeath')) {
+            item.yearOfDeath = (new Date()).getFullYear();
+        }
+    });
+    let ages = inputArray.map(item => item.yearOfDeath - item.yearOfBirth);
+    let sortedAges = ages.sort(sortingFn);
+    let oldestAge = sortedAges[0];
+    return inputArray.find(item => (item.yearOfDeath - item.yearOfBirth) == oldestAge);
 };
+
+function sortingFn(a, b) {
+    if (a > b) {
+        return -1;
+    } else if (a < b) {
+        return 1;
+    }
+    return 0;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
