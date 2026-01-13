e04a10ecc0a6a958be1e40a76fd29f9b1fdda6e3
Tom W <199689467+TomTomJW@users.noreply.github.com>
2026-01-12 20:36:28 -0500

    Finished 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return (a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(inputArray) {
+  return inputArray.reduce((acc, item) => {
+    return acc + item;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(inputArray) {
+  return inputArray.reduce((acc, item) => {
+    return acc * item;
+  });
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+  if (input < 0) {
+    return NaN;
+  }
+  if (input == 0) {
+    return 1;
+  }
+  let fact = 1;
+  for (let i = 1; i <= input; i++) {
+    fact = fact * i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
