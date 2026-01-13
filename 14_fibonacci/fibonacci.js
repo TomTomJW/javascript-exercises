3f9c899b56f1aa314fcfa40614050af7dac04d05
Tom W <199689467+TomTomJW@users.noreply.github.com>
2026-01-13 11:03:24 -0500

    Finished 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    if ((Math.round(input) != input) || input < 0) {
+      return "OOPS";
+    }
+    let fibonacciSequence = [];
+    fibonacciSequence.push(0);
+    fibonacciSequence.push(1);
+    for (let i = 2; i < (input + 1); i++) {
+      let sum = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
+      fibonacciSequence.push(sum);
+    };
+    return fibonacciSequence[input];
 };
 
 // Do not edit below this line
